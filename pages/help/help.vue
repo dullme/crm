2b8cee9b9f4b01e26e019bb4fe08e3695110b7ee
dullme <admin@dullme.com>
2019-09-30 16:15:48 +0800

    添加帮助

---
+++ pages/help/help.vue
@@ -0,0 +1,128 @@
+<template>
+	<view class="body index-body">
+		<div class="message" style="display: flex;align-items: center;background-color: #13182A;padding: 20upx 50upx;">
+			<image style="width: 30upx;height: 30upx;margin-right: 20upx;" src="../../static/notice_icon.png"></image>
+			<span style="font-size: 30upx;color: #6A77A0;">这里是消息</span>
+		</div>
+		<div class="real-body">
+			<div style="font-size: 30upx;border-bottom: 1px solid #323D5E;margin-bottom: 20upx;">
+				<div style="margin-bottom: 20upx;display: flex;align-items: center;justify-content: space-between;">
+					<div style="display: flex;align-items: center;">
+						<div style="margin-right: 20upx;padding: 30upx 50upx;border-radius: 8upx;background-image: url('../../static/help_pic1.png');">
+							<span style="color: #B4B9CA;">提现</span>
+						</div>
+						<div>提现记录问题说明</div>
+					</div>
+					
+					<image style="width: 30upx;height: 30upx;" src="../../static/jump_top.png"></image>
+				</div>
+				<div style="margin-bottom: 20upx;color: #6A77A0;">
+					前几天的GQ举办的一场活动，引来了无数讨论的题。首先就是主持人董又霖的拙劣表演，让不少网友吐槽。随后GQ官方号又曝光了不少幕后
+				</div>
+			</div>
+			
+			<div style="font-size: 30upx;border-bottom: 1px solid #323D5E;margin-bottom: 20upx;">
+				<div style="margin-bottom: 20upx;display: flex;align-items: center;justify-content: space-between;">
+					<div style="display: flex;align-items: center;">
+						<div style="margin-right: 20upx;padding: 30upx 50upx;border-radius: 8upx;background-image: url('../../static/help_pic1.png');">
+							<span style="color: #B4B9CA;">投诉</span>
+						</div>
+						<div>提现记录问题说明</div>
+					</div>
+					
+					<image style="width: 30upx;height: 30upx;" src="../../static/jump_down.png"></image>
+				</div>
+				<div style="display: none;padding: 20upx 0;color: #6A77A0;">
+					前几天的GQ举办的一场活动，引来了无数讨论的题。首先就是主持人董又霖的拙劣表演，让不少网友吐槽。随后GQ官方号又曝光了不少幕后
+				</div>
+			</div>
+			
+			<div style="font-size: 30upx;border-bottom: 1px solid #323D5E;margin-bottom: 20upx;">
+				<div style="margin-bottom: 20upx;display: flex;align-items: center;justify-content: space-between;">
+					<div style="display: flex;align-items: center;">
+						<div style="margin-right: 20upx;padding: 30upx 50upx;border-radius: 8upx;background-image: url('../../static/help_pic1.png');">
+							<span style="color: #B4B9CA;">投诉</span>
+						</div>
+						<div>提现记录问题说明</div>
+					</div>
+					
+					<image style="width: 30upx;height: 30upx;" src="../../static/jump_down.png"></image>
+				</div>
+				<div style="display: none;padding: 20upx 0;color: #6A77A0;">
+					前几天的GQ举办的一场活动，引来了无数讨论的题。首先就是主持人董又霖的拙劣表演，让不少网友吐槽。随后GQ官方号又曝光了不少幕后
+				</div>
+			</div>
+			
+			<div style="font-size: 30upx;border-bottom: 1px solid #323D5E;margin-bottom: 20upx;">
+				<div style="margin-bottom: 20upx;display: flex;align-items: center;justify-content: space-between;">
+					<div style="display: flex;align-items: center;">
+						<div style="margin-right: 20upx;padding: 30upx 50upx;border-radius: 8upx;background-image: url('../../static/help_pic1.png');">
+							<span style="color: #B4B9CA;">投诉</span>
+						</div>
+						<div>提现记录问题说明</div>
+					</div>
+					
+					<image style="width: 30upx;height: 30upx;" src="../../static/jump_down.png"></image>
+				</div>
+				<div style="display: none;padding: 20upx 0;color: #6A77A0;">
+					前几天的GQ举办的一场活动，引来了无数讨论的题。首先就是主持人董又霖的拙劣表演，让不少网友吐槽。随后GQ官方号又曝光了不少幕后
+				</div>
+			</div>
+			
+			<div style="font-size: 30upx;border-bottom: 1px solid #323D5E;margin-bottom: 20upx;">
+				<div style="margin-bottom: 20upx;display: flex;align-items: center;justify-content: space-between;">
+					<div style="display: flex;align-items: center;">
+						<div style="margin-right: 20upx;padding: 30upx 50upx;border-radius: 8upx;background-image: url('../../static/help_pic1.png');">
+							<span style="color: #B4B9CA;">投诉</span>
+						</div>
+						<div>提现记录问题说明</div>
+					</div>
+					
+					<image style="width: 30upx;height: 30upx;" src="../../static/jump_down.png"></image>
+				</div>
+				<div style="display: none;padding: 20upx 0;color: #6A77A0;">
+					前几天的GQ举办的一场活动，引来了无数讨论的题。首先就是主持人董又霖的拙劣表演，让不少网友吐槽。随后GQ官方号又曝光了不少幕后
+				</div>
+			</div>
+			
+			
+		</div>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		},
+
+		methods: {
+
+			back: function (e) {
+				uni.navigateBack()
+			},
+		}
+	}
+</script>
+
+<style>
+	page{
+		background: #19233C;
+	}
+
+	.body{
+		padding: 0;
+		width: 100%;
+		height: 100%;
+		color: white;
+		background: #19233C;
+		color:#E9EAEB;
+	}
+
+	.real-body{
+		margin-top: 60upx;
+		padding: 0upx 50upx 0 50upx;
+	}
+</style>
